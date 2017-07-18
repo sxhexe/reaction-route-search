f7900e3257717d1cd6fb30bfe0a95b52830be158
sxh <sxh@ucmerced-169-236-70-248.ucmerced.edu>
2017-07-18 11:57:43 -0700

    get rid of compiled files

--- openbabel2.4.1Build/scripts/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/reactionroutesearch/reaction-route-search/openbabel-2.4.1/scripts
-# Build directory: /home/reactionroutesearch/reaction-route-search/openbabel2.4.1Build/scripts
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
